3773982c56b417cdb27f55bb744498e5d5237a31
jldorado <xeltjldm@gmail.com>
2020-10-13 14:15:48 +0200

    layouts

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -198,10 +198,31 @@
     <div class="container">
         <div class="row">
             <div class="col-6"><h1 class="container">holaa</h1></div>
-            {{-- <div class="col-6"><a href="{{ route('admin') }}">Panel</a></div> --}}
+            <div class="col-6"><a href="{{ route('admin') }}">Panel</a></div>
         </div>
     </div>
 @endsection
+
+{{-- <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Barra de categorias
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6"><h1 class="container">holaa</h1></div>
+                        <div class="col-6"><a href="{{ route('admin') }}">Panel</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout> --}}
 
 
 
